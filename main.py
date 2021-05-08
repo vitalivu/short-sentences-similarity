0a45539dd5f3b0121e9b1b8062623f39c163221c
Linh Vu Thai <vuthailinh.linh7.v7@gmail.com>
2021-05-08 10:33:45 +0700

    Add application.conf and read config in main app

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import io
-import logging
 import pickle
 from time import perf_counter
 
 import torch
+from pyhocon import ConfigFactory
 from quart import Quart, request
 from quart_cors import cors
 from sentence_transformers import SentenceTransformer, util
 
+config = ConfigFactory.parse_file('application.conf')
+model_name = config.get_string('ss_search.model')
+
 app = Quart(__name__)
 app = cors(app, allow_origin="*")
-model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+model = SentenceTransformer(model_name)
 
 stopwords = set(
     ['a', 'about', 'above', 'after', 'again', 'against', 'ain', 'all', 'am', 'an', 'and', 'any', 'are', 'aren',
@@ -28,22 +31,19 @@ stopwords = set(
      "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves'])
 
 
+def normalize_phases(sentence):
+    ph = config.get('ss_search.norm_phases')
+    for k, v in ph.items():
+        sentence = sentence.replace(k, v)
+    return sentence
+
+
 def clean_text(sent):
     # Removing non ASCII chars
     sent = str(sent).replace(r'[^\x00-\x7f]', r' ')
 
     # Replace some common paraphrases
-    sent_norm = sent.lower() \
-        .replace("how do you", "how do i") \
-        .replace("how do we", "how do i") \
-        .replace("how can we", "how can i") \
-        .replace("how can you", "how can i") \
-        .replace("how can i", "how do i") \
-        .replace("really true", "true") \
-        .replace("what are the importance", "what is the importance") \
-        .replace("what was", "what is") \
-        .replace("so many", "many") \
-        .replace("would it take", "will it take")
+    sent_norm = normalize_phases(sent.lower())
 
     # Remove any punctuation characters
     for c in [",", "!", ".", "?", "'", '"', ":", ";", "[", "]", "{", "}", "<", ">"]:
@@ -53,15 +53,6 @@ def clean_text(sent):
     tokens = sent_norm.split()
     tokens = [token for token in tokens if token not in stopwords]
     return " ".join(tokens)
-
-
-# by default, Pickle does not support load model to cpu
-class CpuUnpickler(pickle.Unpickler):
-    def find_class(self, module, name):
-        if module == 'torch.storage' and name == '_load_from_bytes':
-            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
-        else:
-            return super().find_class(module, name)
 
 
 # on the fly
@@ -80,15 +71,14 @@ async def query():
 def search_for_similar(query):
     print("### Query:", query)
     query_embedding = model.encode(clean_text(query), convert_to_tensor=True)
-    cos_scores = util.pytorch_cos_sim(query_embedding, embeddings2)[0]
+    cos_scores = util.pytorch_cos_sim(query_embedding, embeddings)[0]
     top_results = torch.topk(cos_scores, k=top_k)
-    print("Top similar queries:")
     result = []
     for score, idx in zip(top_results[0], top_results[1]):
-        print("({:.4f})".format(score), question2[idx])
+        print("({:.4f})".format(score), questions[idx])
         result.append({
             'id': idx.item(),
-            'title': question2[idx],
+            'title': questions[idx],
             'score': "{:.4f}".format(score),
             'author': 'Anonymous',
             # 'desc': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a lectus blandit, aliquam magna at, rhoncus nisi. Etiam ultrices, nunc in tempus volutpat, mi nisi tincidunt risus, vel consectetur nisl lorem vel risus. Aenean turpis ligula, consectetur id bibendum ac, maximus et libero. In porta, dui non tristique posuere, dui libero viverra enim, et rutrum odio nunc lobortis libero. In imperdiet purus eu vestibulum vestibulum. Nullam nec rutrum nisl. Sed euismod est sed congue tincidunt. Proin ornare elit aliquet nulla malesuada aliquam."
@@ -96,19 +86,26 @@ def search_for_similar(query):
     return result
 
 
+# by default, Pickle does not support load model to cpu
+class CpuUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else:
+            return super().find_class(module, name)
+
+
+datafile = config.get_string('ss_search.datafile')
 t1 = perf_counter()
-# Load sentences & embeddings from disc
-with open('embeddings.pkl', "rb") as fIn:
+with open(datafile, "rb") as fIn:
     stored_data = CpuUnpickler(fIn).load()
-    # question1 = stored_data['sentences']
-    # embeddings1 = stored_data['embeddings']
-    question2 = stored_data['sentences2']
-    embeddings2 = stored_data['embeddings2']
+    questions = stored_data['questions']
+    embeddings = stored_data['embeddings']
 t2 = perf_counter()
 
 print("Took {:.2f} seconds to import model".format(t2 - t1))
-top_k = min(10, len(embeddings2))
+top_k = min(10, len(embeddings))
 
 if __name__ == "__main__":
     search_for_similar('What is the approx annual cost of living while studying in UIC Chicago, for an Indian student?')
-    app.run(debug=True)
+    app.run(debug=False)
